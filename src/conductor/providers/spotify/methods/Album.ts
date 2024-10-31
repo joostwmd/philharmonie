dbb39ace8bdbb455cfe9fa9a42845f4c963e6929
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-10-31 16:05:56 +0100

    introduced typing

--- src/conductor/providers/spotify/methods/Album.ts
+++ src/conductor/providers/spotify/methods/Album.ts
@@ -1,5 +1,6 @@
 import { makeGetRequest } from '../../../../utils';
 import { SPOTIFY_API_BASE_URL, SPOTIFY_METHODS_PATHS } from '../constants';
+import type { ISpotifyAlbum } from '../types';
 
 export class Album {
   private apiKey: string;
@@ -8,7 +9,7 @@ export class Album {
     this.apiKey = apiKey;
   }
 
-  async getById(albumId: string): Promise<string> {
+  async getById(albumId: string): Promise<ISpotifyAlbum> {
     console.log('spotify.album.getById', albumId);
     const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.album}${albumId}`;
     console.log('url', url);
