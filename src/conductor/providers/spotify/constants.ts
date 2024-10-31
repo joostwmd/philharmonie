d2309700a213e55ed85cba4bea5b9b5dd70a8054
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-10-31 16:03:26 +0100

    handels api paths as constants

---
+++ src/conductor/providers/spotify/constants.ts
@@ -0,0 +1,12 @@
+export const SPOTIFY_API_BASE_URL = 'https://api.spotify.com/v1/';
+export const SPOTIFY_METHODS_PATHS = {
+  user: 'users/',
+  current_user: 'me/',
+  album: 'albums/',
+  playlist: 'playlists/',
+  track: 'tracks/',
+  artist: 'artists/',
+  search: 'search',
+  recommendations: 'recommendations',
+  audio_features: 'audio-features',
+};
