78714ca9d484c2706c7b6188fefd47f6a5c512fe
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-10-31 18:14:11 +0100

    added spotify artist

--- src/conductor/providers/spotify/types.ts
+++ src/conductor/providers/spotify/types.ts
@@ -140,3 +140,25 @@ export interface ISpotifyAlbumTracksResponse {
     is_local: boolean;
   }>;
 }
+
+export interface ISpotifyArtist {
+  external_urls: {
+    spotify: string;
+  };
+  followers: {
+    href: string;
+    total: number;
+  };
+  genres: string[];
+  href: string;
+  id: string;
+  images: Array<{
+    url: string;
+    height: number;
+    width: number;
+  }>;
+  name: string;
+  popularity: number;
+  type: string;
+  uri: string;
+}
