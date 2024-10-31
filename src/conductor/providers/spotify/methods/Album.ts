13a0c59d7689ece885827822a097f3d613838276
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-10-31 17:11:16 +0100

    uses makeRequestFunc and added more endpoints

--- src/conductor/providers/spotify/methods/Album.ts
+++ src/conductor/providers/spotify/methods/Album.ts
@@ -1,6 +1,6 @@
-import { makeGetRequest } from '../../../../utils';
+import { makeRequest } from '../../../../utils';
 import { SPOTIFY_API_BASE_URL, SPOTIFY_METHODS_PATHS } from '../constants';
-import type { ISpotifyAlbum } from '../types';
+import type { ISpotifyAlbum, ISpotifyAlbumTracksResponse } from '../types';
 
 export class Album {
   private apiKey: string;
@@ -11,10 +11,47 @@ export class Album {
 
   async getById(albumId: string): Promise<ISpotifyAlbum> {
     console.log('spotify.album.getById', albumId);
-    const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.album}${albumId}`;
-    console.log('url', url);
-    return await makeGetRequest(url, this.apiKey, 'spotfiy');
+    const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.albums}${albumId}`;
+    return await makeRequest(url, this.apiKey, 'spotfiy');
   }
 
-  // Add more album-related methods here as needed
+  async getSeveralById(albumIds: string[]): Promise<ISpotifyAlbum[]> {
+    console.log('spotify.album.getSeveralById', albumIds);
+    const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.albums}`;
+    return await makeRequest(url, this.apiKey, 'spotfiy');
+  }
+
+  async getTracks(albumId: string): Promise<ISpotifyAlbumTracksResponse> {
+    console.log('spotify.album.getTracks', albumId);
+    const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.albums}${albumId}/tracks`;
+    return await makeRequest(url, this.apiKey, 'spotfiy');
+  }
+
+  async getUsersSavedAlbums(): Promise<ISpotifyAlbum[]> {
+    console.log('spotify.album.getUsersSavedAlbums');
+    const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.current_user}${SPOTIFY_METHODS_PATHS.albums}`;
+    return await makeRequest(url, this.apiKey, 'spotfiy');
+  }
+
+  async saveAlbumForUser(albumId: string): Promise<void> {
+    console.log('spotify.album.saveAlbum', albumId);
+    const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.current_user}${SPOTIFY_METHODS_PATHS.albums}${albumId}`;
+    //make PUT request
+    //await makeGetRequest(url, this.apiKey, 'spotfiy');
+    return await makeRequest(url, this.apiKey, 'spotfiy', 'PUT');
+  }
+
+  async removeAlbumForUser(albumId: string): Promise<void> {
+    console.log('spotify.album.removeAlbum', albumId);
+    const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.current_user}${SPOTIFY_METHODS_PATHS.albums}${albumId}`;
+    //make DELETE request
+    //await makeGetRequest(url, this.apiKey, 'spotfiy');
+    return await makeRequest(url, this.apiKey, 'spotfiy', 'DELETE');
+  }
+
+  async checkUsersSavedAlbums(albumIds: string[]): Promise<boolean[]> {
+    console.log('spotify.album.checkUsersSavedAlbums', albumIds);
+    const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.current_user}${SPOTIFY_METHODS_PATHS.albums}contains`;
+    return await makeRequest(url, this.apiKey, 'spotfiy');
+  }
 }
