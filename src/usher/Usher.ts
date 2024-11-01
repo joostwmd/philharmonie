ed6c2bc710d9dcff218dd63547ad8ca9789cc5f8
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-11-01 19:40:46 +0100

    removed current provider logic in favor of module strucutre

--- src/usher/Usher.ts
+++ src/usher/Usher.ts
@@ -8,58 +8,9 @@ import type { IUsherProvider } from './providers/types';
 import { SpotifyUsherProvider } from './providers/spotify';
 
 export class Usher implements IUsher {
-  private currentProvider: IUsherProvider | null;
-  private providers: Partial<Record<TUsherProviders, IUsherProvider>>;
+  public spotify: SpotifyUsherProvider;
 
-  constructor(providers: TUsherProviderConfig[]) {
-    this.currentProvider = null;
-    this.providers = {};
-    this.initProviders(providers);
+  constructor(providersConfigs: TUsherProviderConfig[]) {
+    this.spotify = new SpotifyUsherProvider();
   }
-
-  private initProviders(providers: TUsherProviderConfig[]): void {
-    providers.forEach((provider) => {
-      switch (provider.name) {
-        case 'spotify':
-          const spotifyProvider = new SpotifyUsherProvider() as IUsherProvider;
-          spotifyProvider.initWithCredentials(provider);
-          this.providers[provider.name] = spotifyProvider;
-          break;
-        default:
-          throw new Error(`Unknown provider: ${provider}`);
-      }
-    });
-  }
-
-  private setCurrentProvider(providerName: TUsherProviders): void {
-    if (this.providers[providerName]) {
-      this.currentProvider = this.providers[providerName]!;
-    } else {
-      throw new Error(`Provider ${providerName} is not available`);
-    }
-  }
-
-  public async refreshSession(
-    providerName: TUsherProviders,
-    refreshToken: string,
-  ): Promise<TSession> {
-    this.setCurrentProvider(providerName);
-    if (!this.currentProvider) {
-      throw new Error('No provider selected');
-    }
-    return await this.currentProvider.refreshSession(refreshToken);
-  }
-
-  public createSession = {
-    withClientCredentials: async (
-      providerName: TUsherProviders,
-    ): Promise<TSession> => {
-      this.setCurrentProvider(providerName);
-      if (!this.currentProvider) {
-        throw new Error('No provider selected');
-      }
-
-      return await this.currentProvider.createSession.withClientCredentials();
-    },
-  };
 }
