7f9b4ed4ad0b230c44a12e851114f0e2d6463be6
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-11-02 21:50:35 +0100

    Refactor Spotify provider constructor to use a config object

--- src/conductor/providers/spotify/index.ts
+++ src/conductor/providers/spotify/index.ts
@@ -5,6 +5,7 @@ import { Track } from './methods/Track';
 import { Artist } from './methods/Artist';
 import { Search } from './methods/Search';
 import { ConductorProvider } from '../Provider';
+import type { TConductorProviderConfig } from '../../types';
 
 export class Spotify extends ConductorProvider {
   public album: Album;
@@ -14,8 +15,8 @@ export class Spotify extends ConductorProvider {
   public artist: Artist;
   public search: Search;
 
-  constructor(apiToken: string) {
-    super(apiToken, 'spotify');
+  constructor(providerConfig: TConductorProviderConfig<'spotify'>) {
+    super('spotify', providerConfig);
 
     this.album = new Album(this);
     this.playlist = new Playlist(this);
