df25d90fe114c0afb75a9e75976300946b9f5140
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-11-02 20:29:44 +0100

    utilizes the makeRequest function

--- src/conductor/providers/spotify/methods/Album.ts
+++ src/conductor/providers/spotify/methods/Album.ts
@@ -1,4 +1,4 @@
-import { makeRequest } from '../../../../utils';
+import type { Spotify } from '..';
 import { SPOTIFY_API_BASE_URL, SPOTIFY_METHODS_PATHS } from '../constants';
 import type { TGetByIdInput } from '../types/input';
 import type {
@@ -9,56 +9,56 @@ import type {
 } from '../types/response';
 
 export class Album {
-  private apiKey: string;
+  private provider: Spotify;
 
-  constructor(apiKey: string) {
-    this.apiKey = apiKey;
+  constructor(provider: Spotify) {
+    this.provider = provider;
   }
 
   async getById(albumId: TGetByIdInput): Promise<TGetAlbumByIdResponse> {
     console.log('spotify.album.getById', albumId);
     const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.albums}${albumId}`;
-    return await makeRequest(url, this.apiKey, 'spotify');
+    return await this.provider.makeRequest(url);
   }
 
-  async getSeveralById(
-    albumIds: TGetByIdInput[],
-  ): Promise<TGetSeveralAlbumsByIdsResponse> {
-    const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.albums}?ids=${encodeURIComponent(albumIds.join(','))}`;
-    return await makeRequest(url, this.apiKey, 'spotify');
-  }
+  // async getSeveralById(
+  //   albumIds: TGetByIdInput[],
+  // ): Promise<TGetSeveralAlbumsByIdsResponse> {
+  //   const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.albums}?ids=${encodeURIComponent(albumIds.join(','))}`;
+  //   return await makeRequest(url, this.apiKey, 'spotify');
+  // }
 
-  async getTracks(albumId: TGetByIdInput): Promise<TGetAlbumTracksResponse> {
-    console.log('spotify.album.getTracks', albumId);
-    const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.albums}${albumId}/tracks`;
-    return await makeRequest(url, this.apiKey, 'spotify');
-  }
+  // async getTracks(albumId: TGetByIdInput): Promise<TGetAlbumTracksResponse> {
+  //   console.log('spotify.album.getTracks', albumId);
+  //   const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.albums}${albumId}/tracks`;
+  //   return await makeRequest(url, this.apiKey, 'spotify');
+  // }
 
-  async getUsersSavedAlbums(): Promise<TGetUsersSavedAlbumsResponse> {
-    console.log('spotify.album.getUsersSavedAlbums');
-    const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.current_user}${SPOTIFY_METHODS_PATHS.albums}`;
-    return await makeRequest(url, this.apiKey, 'spotify');
-  }
+  // async getUsersSavedAlbums(): Promise<TGetUsersSavedAlbumsResponse> {
+  //   console.log('spotify.album.getUsersSavedAlbums');
+  //   const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.current_user}${SPOTIFY_METHODS_PATHS.albums}`;
+  //   return await makeRequest(url, this.apiKey, 'spotify');
+  // }
 
-  async saveAlbumsForUser(albumIds: string[]): Promise<void> {
-    console.log('spotify.album.saveAlbum', albumIds);
-    const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.current_user}${SPOTIFY_METHODS_PATHS.albums}`;
-    await makeRequest(url, this.apiKey, 'spotify', 'PUT', { ids: [albumIds] });
-  }
+  // async saveAlbumsForUser(albumIds: string[]): Promise<void> {
+  //   console.log('spotify.album.saveAlbum', albumIds);
+  //   const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.current_user}${SPOTIFY_METHODS_PATHS.albums}`;
+  //   await makeRequest(url, this.apiKey, 'spotify', 'PUT', { ids: [albumIds] });
+  // }
 
-  async removeAlbumForUser(albumId: TGetByIdInput): Promise<void> {
-    console.log('spotify.album.removeAlbum', albumId);
-    const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.current_user}${SPOTIFY_METHODS_PATHS.albums}${albumId}`;
-    await makeRequest(url, this.apiKey, 'spotify', 'DELETE', {
-      ids: [albumId],
-    });
-  }
+  // async removeAlbumForUser(albumId: TGetByIdInput): Promise<void> {
+  //   console.log('spotify.album.removeAlbum', albumId);
+  //   const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.current_user}${SPOTIFY_METHODS_PATHS.albums}${albumId}`;
+  //   await makeRequest(url, this.apiKey, 'spotify', 'DELETE', {
+  //     ids: [albumId],
+  //   });
+  // }
 
-  async checkUsersSavedAlbums(albumIds: TGetByIdInput[]): Promise<boolean[]> {
-    console.log('spotify.album.checkUsersSavedAlbums', albumIds);
-    const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.current_user}${SPOTIFY_METHODS_PATHS.albums}contains`;
-    return await makeRequest(url, this.apiKey, 'spotify', 'GET', {
-      ids: albumIds,
-    });
-  }
+  // async checkUsersSavedAlbums(albumIds: TGetByIdInput[]): Promise<boolean[]> {
+  //   console.log('spotify.album.checkUsersSavedAlbums', albumIds);
+  //   const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.current_user}${SPOTIFY_METHODS_PATHS.albums}contains`;
+  //   return await makeRequest(url, this.apiKey, 'spotify', 'GET', {
+  //     ids: albumIds,
+  //   });
+  // }
 }
