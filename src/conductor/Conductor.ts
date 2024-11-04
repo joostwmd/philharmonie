a86f005610f5a19c5a4a249fe2031bfc28ca05ad
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-11-04 18:08:47 +0100

    Refactor Conductor.ts to allow for partial configuration of providers and added createConductor function, that returns Conductor instance with typing based on provided configuration.

--- src/conductor/Conductor.ts
+++ src/conductor/Conductor.ts
@@ -1,13 +1,24 @@
 import { Spotify } from './providers/spotify';
 import { AppleMusic } from './providers/appleMusic';
-import type { TConductorConfig } from './types';
+import type { TConductor, TConductorProvidersConfig } from './types';
 
-export class Conductor {
-  public spotify: Spotify;
-  public appleMusic: AppleMusic;
+export class Conductor<Config extends TConductorProvidersConfig> {
+  public spotify?: Spotify;
+  public appleMusic?: AppleMusic;
 
-  constructor(config: TConductorConfig, fetch: any) {
-    this.spotify = new Spotify(config.spotify);
-    this.appleMusic = new AppleMusic(config.appleMusic);
+  constructor(config: Config) {
+    if (config.spotify) {
+      this.spotify = new Spotify(config.spotify);
+    }
+    if (config.appleMusic) {
+      this.appleMusic = new AppleMusic(config.appleMusic);
+    }
   }
+}
+
+export function createConductor<Config extends TConductorProvidersConfig>(
+  config: Config,
+): TConductor<Config> {
+  const conductor = new Conductor(config) as TConductor<Config>;
+  return conductor;
 }
