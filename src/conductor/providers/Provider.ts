b2a70e58375c64080bc006659e8056d9f6a28f86
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-11-05 20:28:19 +0100

    Refactor ConductorProvider to allow custom fetch function and passes it to handleMake Request

--- src/conductor/providers/Provider.ts
+++ src/conductor/providers/Provider.ts
@@ -10,14 +10,17 @@ export abstract class ConductorProvider {
   private tokens: TOAuthApiTokens | TAppleMusicApiTokens;
   private providerName: string;
   public market: string;
+  private fetch: typeof fetch;
 
   constructor(
     providerName: TConductorProviders,
     providerConfig: TConductorProviderConfig<TConductorProviders>,
+    fetchFunction?: typeof fetch,
   ) {
     this.providerName = providerName;
     this.tokens = providerConfig.tokens;
     this.market = providerConfig.defaultMarket;
+    this.fetch = fetchFunction || fetch;
   }
 
   public abstract setUserMarket(): Promise<void>;
@@ -33,6 +36,7 @@ export abstract class ConductorProvider {
       this.providerName,
       method,
       body,
+      this.fetch,
     );
   }
 }
