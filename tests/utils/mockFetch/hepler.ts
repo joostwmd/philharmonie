1cf0c78cbec76b0cbf25739cb2a0faeb14b11dec
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-11-05 21:15:43 +0100

    helper funciton to check the method and body and to throw a mock error

--- tests/utils/mockFetch/hepler.ts
+++ tests/utils/mockFetch/hepler.ts
@@ -5,3 +5,25 @@ export function returnMockResponse(data: any) {
     json: async () => data,
   };
 }
+
+export function returnMockErrorResponse(status: number, message: string) {
+  return {
+    ok: false,
+    status: status,
+    statusText: message,
+    json: async () => ({ error: message }),
+  };
+}
+
+export function isCorrectMethodUsed(
+  intendedMethod: string,
+  usedMethod: string | undefined,
+): boolean {
+  return intendedMethod === usedMethod;
+}
+
+export function isBodyCorrect(intendedBody: any, usedBody: any) {
+  if (intendedBody !== usedBody) {
+    throw new Error(`Expected body ${intendedBody} but got ${usedBody}`);
+  }
+}
