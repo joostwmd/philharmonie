843d0cd1dba305e9961c922722b173c40d7a7a13
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-11-06 14:32:02 +0100

    added helper functions to validate the request

--- tests/utils/mockFetch/hepler.ts
+++ tests/utils/mockFetch/hepler.ts
@@ -1,4 +1,4 @@
-export function returnMockResponse(data: any) {
+export function createMockSuccessResponse(data: any) {
   return {
     ok: true,
     status: 200,
@@ -6,7 +6,7 @@ export function returnMockResponse(data: any) {
   };
 }
 
-export function returnMockErrorResponse(status: number, message: string) {
+export function createMockErrorResponse(status: number, message: string) {
   return {
     ok: false,
     status: status,
@@ -15,15 +15,58 @@ export function returnMockErrorResponse(status: number, message: string) {
   };
 }
 
-export function isCorrectMethodUsed(
+export function validateHttpMethod(
   intendedMethod: string,
   usedMethod: string | undefined,
 ): boolean {
   return intendedMethod === usedMethod;
 }
 
-export function isBodyCorrect(intendedBody: any, usedBody: any) {
-  if (intendedBody !== usedBody) {
-    throw new Error(`Expected body ${intendedBody} but got ${usedBody}`);
+export function validateRequestBody(intendedBody: any, usedBody: any) {
+  return JSON.stringify(intendedBody) === JSON.stringify(usedBody);
+}
+
+export function validateUrlParams(intendedParams: string[], url: URL): boolean {
+  const usedParams = url.searchParams;
+
+  for (const param of intendedParams) {
+    if (!usedParams.has(param)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export function validateRequest(
+  intendedMethod: string,
+  usedMethod: string | undefined,
+  intendedParams: string[],
+  url: string,
+  intendedBody?: any,
+  usedBody?: any,
+): void | ReturnType<typeof createMockErrorResponse> {
+  if (!validateHttpMethod(intendedMethod, usedMethod)) {
+    return createMockErrorResponse(
+      405,
+      'Mismatch between intended and used method',
+    );
+  }
+
+  if (!validateUrlParams(intendedParams, new URL(url))) {
+    return createMockErrorResponse(
+      400,
+      'Mismatch between intended and used params',
+    );
+  }
+
+  if (
+    intendedBody &&
+    usedBody &&
+    !validateRequestBody(intendedBody, usedBody)
+  ) {
+    return createMockErrorResponse(
+      400,
+      'Mismatch between intended and used body',
+    );
   }
 }
