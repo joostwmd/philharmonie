7b61522b871260f2a3d5744957581fc82ed663cb
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-11-06 16:35:25 +0100

    added mock input data

---
+++ tests/utils/mockData/spotify/input/index.ts
@@ -0,0 +1,13 @@
+import type { TLimitAndOffsetOptions } from '../../../../../src/conductor/providers/spotify/types/input';
+
+export const spotifyAlbumIdSample = '01S3g4PCFaRQMfwiAm4K2H';
+export const spotifyAlbumIdsSample = ['01S3g4PCFaRQMfwiAm4K2H'];
+export const spotifyArtistIdSample = '6sgeY4vovk7KfxVwiu2V9E';
+export const spotifyPlaylistIdSample = '5k8xjLPpBzmVuTSGtSUqcp';
+export const spotifyTrackIdSample = '11dFghVXANMlKmJXsNCbNl';
+export const spotifyUserIdSample = '717ut5w9hmph9ckf8y81n3q4w';
+
+export const spotifyLimitAndOffsetOptionsSample: TLimitAndOffsetOptions = {
+  offset: 0,
+  limit: 10,
+};
