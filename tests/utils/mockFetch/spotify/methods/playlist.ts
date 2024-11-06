a229d249bddf16d94c22fc0a17a5463ed4064ea1
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-11-06 14:32:51 +0100

    implemented validateRequest function

--- tests/utils/mockFetch/spotify/methods/playlist.ts
+++ tests/utils/mockFetch/spotify/methods/playlist.ts
@@ -5,7 +5,8 @@ import {
   spotifyListOfUsersPlaylistsResponse,
   spotifyCreatePlaylistResponse,
 } from '../../../mockData/spotify/responses/playlist';
-import { returnMockResponse } from '../../hepler';
+import { SPOTIFY_URL_PARAMS } from '../../constants';
+import { createMockSuccessResponse, validateRequest } from '../../hepler';
 
 export async function handleSpotifyPlaylistMockRequests(
   url: string,
@@ -14,57 +15,119 @@ export async function handleSpotifyPlaylistMockRequests(
   // Playlists
   if (url.match(/\/v1\/playlists\/[^/]+$/) && options?.method === 'GET') {
     // get playlist by id
-    return returnMockResponse(spotifySinglePlaylistResponse);
+    const errorResponse = validateRequest(
+      'GET',
+      options?.method,
+      [
+        SPOTIFY_URL_PARAMS.market,
+        SPOTIFY_URL_PARAMS.fields,
+        SPOTIFY_URL_PARAMS.additional_types,
+      ],
+      url,
+    );
+    if (errorResponse) return errorResponse;
+    return createMockSuccessResponse(spotifySinglePlaylistResponse);
   } else if (
     url.match(/\/v1\/playlists\/[^/]+$/) &&
     options?.method === 'PUT'
   ) {
     // change playlist details
-    return returnMockResponse(null);
+    const errorResponse = validateRequest('PUT', options?.method, [], url);
+    if (errorResponse) return errorResponse;
+    return createMockSuccessResponse(null);
   } else if (
     url.match(/\/v1\/playlists\/[^/]+\/tracks$/) &&
     options?.method === 'GET'
   ) {
     // get playlist items
-    return returnMockResponse(spotifyPlaylistSnapshotResponse);
+    const errorResponse = validateRequest(
+      'GET',
+      options?.method,
+      [
+        SPOTIFY_URL_PARAMS.market,
+        SPOTIFY_URL_PARAMS.fields,
+        SPOTIFY_URL_PARAMS.limit,
+        SPOTIFY_URL_PARAMS.offset,
+        SPOTIFY_URL_PARAMS.additional_types,
+      ],
+      url,
+    );
+    if (errorResponse) return errorResponse;
+    return createMockSuccessResponse(spotifyPlaylistSnapshotResponse);
   } else if (
     url.match(/\/v1\/playlists\/[^/]+\/tracks$/) &&
     options?.method === 'PUT'
   ) {
     // update playlist items
-    return returnMockResponse(spotifyPlaylistSnapshotResponse);
+    const errorResponse = validateRequest(
+      'PUT',
+      options?.method,
+      [SPOTIFY_URL_PARAMS.uris],
+      url,
+    );
+    if (errorResponse) return errorResponse;
+    return createMockSuccessResponse(spotifyPlaylistSnapshotResponse);
   } else if (
     url.match(/\/v1\/playlists\/[^/]+\/tracks$/) &&
     options?.method === 'POST'
   ) {
     // add items to playlist
-    return returnMockResponse(spotifyPlaylistSnapshotResponse);
+    const errorResponse = validateRequest(
+      'POST',
+      options?.method,
+      [SPOTIFY_URL_PARAMS.position, SPOTIFY_URL_PARAMS.uris],
+      url,
+    );
+    if (errorResponse) return errorResponse;
+    return createMockSuccessResponse(spotifyPlaylistSnapshotResponse);
   } else if (
     url.match(/\/v1\/playlists\/[^/]+\/tracks$/) &&
     options?.method === 'DELETE'
   ) {
     // remove playlist items
-    return returnMockResponse(spotifyPlaylistSnapshotResponse);
+    const errorResponse = validateRequest('DELETE', options?.method, [], url);
+    if (errorResponse) return errorResponse;
+    return createMockSuccessResponse(spotifyPlaylistSnapshotResponse);
   } else if (url.includes('/v1/me/playlists') && options?.method === 'GET') {
     // get current user's playlists
-    return returnMockResponse(spotifyListOfCurrentUsersPlaylistsResponse);
+    const errorResponse = validateRequest(
+      'GET',
+      options?.method,
+      [SPOTIFY_URL_PARAMS.limit, SPOTIFY_URL_PARAMS.offset],
+      url,
+    );
+    if (errorResponse) return errorResponse;
+    return createMockSuccessResponse(
+      spotifyListOfCurrentUsersPlaylistsResponse,
+    );
   } else if (
     url.match(/\/v1\/users\/[^/]+\/playlists$/) &&
     options?.method === 'GET'
   ) {
     // get user's playlists
-    return returnMockResponse(spotifyListOfUsersPlaylistsResponse);
+    const errorResponse = validateRequest(
+      'GET',
+      options?.method,
+      [SPOTIFY_URL_PARAMS.limit, SPOTIFY_URL_PARAMS.offset],
+      url,
+    );
+    if (errorResponse) return errorResponse;
+    return createMockSuccessResponse(spotifyListOfUsersPlaylistsResponse);
   } else if (
     url.match(/\/v1\/users\/[^/]+\/playlists$/) &&
     options?.method === 'POST'
   ) {
     // create playlist
-    return returnMockResponse(spotifyCreatePlaylistResponse);
+    const errorResponse = validateRequest('POST', options?.method, [], url);
+    if (errorResponse) return errorResponse;
+    return createMockSuccessResponse(spotifyCreatePlaylistResponse);
   } else if (
     url.match(/\/v1\/playlists\/[^/]+\/images$/) &&
     options?.method === 'PUT'
   ) {
     // add cover image to playlist
-    return returnMockResponse(null);
+    const errorResponse = validateRequest('PUT', options?.method, [], url);
+    if (errorResponse) return errorResponse;
+    return createMockSuccessResponse(null);
   }
 }
