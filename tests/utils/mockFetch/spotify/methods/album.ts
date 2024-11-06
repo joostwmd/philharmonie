52feb94c475daf5cca086befae33ee35dfc62946
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-11-06 16:36:50 +0100

    fixed conditionals so it handels the correct case based in the request and uses input object for validateRequest function to make it more readable

--- tests/utils/mockFetch/spotify/methods/album.ts
+++ tests/utils/mockFetch/spotify/methods/album.ts
@@ -1,3 +1,4 @@
+import { spotifyAlbumIdsSample } from '../../../mockData/spotify/input';
 import {
   spotifyAlbumTracksResponse,
   spotifySeveralAlbumsResponse,
@@ -12,90 +13,105 @@ export function handleSpotifyAlbumMockRequests(
   url: string,
   options?: RequestInit,
 ) {
-  if (url.match(/\/v1\/albums\/[^/]+$/)) {
+  if (
+    url.includes('/v1/albums/') &&
+    !url.includes('/tracks') &&
+    !url.includes('?ids=')
+  ) {
+    console.log('mock fetch spotify single album');
     // single album
-    const errorResponse = validateRequest(
-      'GET',
-      options?.method,
-      [SPOTIFY_URL_PARAMS.market],
+    const errorResponse = validateRequest({
+      intendedMethod: 'GET',
+      usedMethod: options?.method,
+      intendedParams: [SPOTIFY_URL_PARAMS.market],
       url,
-    );
+    });
     if (errorResponse) return errorResponse;
     return createMockSuccessResponse(spotifySingleAlbumResponse);
   } else if (url.includes('/v1/albums?ids=')) {
+    console.log('mock fetch spotify several albums');
     // multiple albums
-    const errorResponse = validateRequest(
-      'GET',
-      options?.method,
-      [SPOTIFY_URL_PARAMS.market, SPOTIFY_URL_PARAMS.ids],
+    const errorResponse = validateRequest({
+      intendedMethod: 'GET',
+      usedMethod: options?.method,
+      intendedParams: [SPOTIFY_URL_PARAMS.market, SPOTIFY_URL_PARAMS.ids],
       url,
-    );
+    });
     if (errorResponse) return errorResponse;
     return createMockSuccessResponse(spotifySeveralAlbumsResponse);
-  } else if (url.match(/\/v1\/albums\/[^/]+\/tracks$/)) {
+  } else if (url.includes('/tracks')) {
+    console.log('mock fetch spotify album tracks');
     // album tracks
-    const errorResponse = validateRequest(
-      'GET',
-      options?.method,
-      [
+    const errorResponse = validateRequest({
+      intendedMethod: 'GET',
+      usedMethod: options?.method,
+      intendedParams: [
         SPOTIFY_URL_PARAMS.market,
         SPOTIFY_URL_PARAMS.limit,
         SPOTIFY_URL_PARAMS.offset,
       ],
       url,
-    );
+    });
     if (errorResponse) return errorResponse;
     return createMockSuccessResponse(spotifyAlbumTracksResponse);
-  } else if (url.includes('/v1/me/albums') && options?.method === 'GET') {
+  } else if (
+    url.includes('/v1/me/albums') &&
+    options?.method === 'GET' &&
+    !url.includes('contains')
+  ) {
+    console.log('mock fetch spotify user saved albums');
     // get user's saved albums
-    const errorResponse = validateRequest(
-      'GET',
-      options?.method,
-      [
+    const errorResponse = validateRequest({
+      intendedMethod: 'GET',
+      usedMethod: options?.method,
+      intendedParams: [
         SPOTIFY_URL_PARAMS.market,
         SPOTIFY_URL_PARAMS.limit,
         SPOTIFY_URL_PARAMS.offset,
       ],
       url,
-    );
+    });
     if (errorResponse) return errorResponse;
     return createMockSuccessResponse(spotifyUsersSavedAlbumsResponse);
-  } else if (url.includes('/v1/me/albums?ids=') && options?.method === 'PUT') {
+  } else if (url.includes('/v1/me/albums') && options?.method === 'PUT') {
+    console.log('mock fetch spotify save album for user', options.body);
+
     // save albums for user
-    const errorResponse = validateRequest(
-      'PUT',
-      options?.method,
-      [SPOTIFY_URL_PARAMS.ids],
+    const errorResponse = validateRequest({
+      intendedMethod: 'PUT',
+      usedMethod: options?.method,
+      intendedParams: [],
       url,
-      //intended body
-      options.body,
-    );
+      intendedBody: { ids: spotifyAlbumIdsSample },
+      usedBody: options.body,
+    });
     if (errorResponse) return errorResponse;
     return createMockSuccessResponse(null);
-  } else if (
-    url.includes('/v1/me/albums?ids=') &&
-    options?.method === 'DELETE'
-  ) {
+  } else if (url.includes('/v1/me/albums') && options?.method === 'DELETE') {
+    console.log('mock fetch spotify remove album for user');
     // remove albums for user
-    const errorResponse = validateRequest(
-      'DELETE',
-      options?.method,
-      [SPOTIFY_URL_PARAMS.ids],
+    const errorResponse = validateRequest({
+      intendedMethod: 'DELETE',
+      usedMethod: options?.method,
+      intendedParams: [],
       url,
-      //intended body
-      options.body,
-    );
+      usedBody: options.body,
+      intendedBody: { ids: [spotifyAlbumIdsSample] },
+    });
     if (errorResponse) return errorResponse;
     return createMockSuccessResponse(null);
   } else if (url.includes('/v1/me/albums/contains?ids=')) {
+    console.log('mock fetch spotify check user saved albums');
     // check user's saved albums
-    const errorResponse = validateRequest(
-      'GET',
-      options?.method,
-      [SPOTIFY_URL_PARAMS.ids],
+    const errorResponse = validateRequest({
+      intendedMethod: 'GET',
+      usedMethod: options?.method,
+      intendedParams: [SPOTIFY_URL_PARAMS.ids],
       url,
-    );
+    });
     if (errorResponse) return errorResponse;
     return createMockSuccessResponse(spotifyCheckUserSavedAlbumsResponse);
   }
+
+  console.error('unhandled spotify album request', url);
 }
