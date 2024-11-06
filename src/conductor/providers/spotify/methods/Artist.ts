316d461822b7432ebadacfe9ac9357821cfabf8f
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-11-06 21:35:42 +0100

    updated input types for spotify artists methods

--- src/conductor/providers/spotify/methods/Artist.ts
+++ src/conductor/providers/spotify/methods/Artist.ts
@@ -1,6 +1,6 @@
 import type { SpotifyConductorProvider } from '..';
 import { SPOTIFY_API_BASE_URL, SPOTIFY_METHODS_PATHS } from '../constants';
-import type { TGetByIdInput } from '../types/input';
+import type { TGetArtistAlbumsOptions, TGetByIdInput } from '../types/input';
 import type { SpotifyApi } from '../types/typed';
 
 export class Artist {
@@ -10,20 +10,34 @@ export class Artist {
     this.provider = provider;
   }
   async getById(artistId: string): Promise<SpotifyApi.SingleArtistResponse> {
-    const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.artists}${artistId}`;
+    const url = `${SPOTIFY_API_BASE_URL}artists/${artistId}`;
     return await this.provider.makeRequest(url);
   }
 
   async getSeveralById(
     artistIds: string[],
   ): Promise<SpotifyApi.MultipleArtistsResponse> {
-    const url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.albums}?ids=${encodeURIComponent(artistIds.join(','))}`;
+    const url = `${SPOTIFY_API_BASE_URL}artists/?ids=${encodeURIComponent(artistIds.join(','))}`;
     return await this.provider.makeRequest(url);
   }
 
-  async getAlbums(artistId: string): Promise<SpotifyApi.ArtistsAlbumsResponse> {
+  async getAlbums(
+    artistId: string,
+    options: TGetArtistAlbumsOptions,
+  ): Promise<SpotifyApi.ArtistsAlbumsResponse> {
     let url = `${SPOTIFY_API_BASE_URL}${SPOTIFY_METHODS_PATHS.artists}${artistId}/albums`;
     url = this.provider.injectMarketIntoUrl(url);
+
+    const params: Record<string, string | number | undefined> = {
+      limit: options.limit,
+      offset: options.offset,
+    };
+
+    if (options.include_groups) {
+      params.include_groups = options.include_groups.join(',');
+    }
+
+    url = this.provider.injectParamsIntoUrl(url, params);
     return await this.provider.makeRequest(url);
   }
 
