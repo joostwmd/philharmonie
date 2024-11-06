21077a31370419c9096646283c19184e44a35d13
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-11-06 16:37:03 +0100

    working tests for albums

--- tests/integration/conductor/spotify/album.test.ts
+++ tests/integration/conductor/spotify/album.test.ts
@@ -1,4 +1,9 @@
 import {
+  spotifyAlbumIdSample,
+  spotifyAlbumIdsSample,
+  spotifyLimitAndOffsetOptionsSample,
+} from '../../../utils/mockData/spotify/input';
+import {
   spotifyAlbumTracksResponse,
   spotifySeveralAlbumsResponse,
   spotifySingleAlbumResponse,
@@ -10,45 +15,53 @@ import { describe, expect, it } from 'vitest';
 
 describe('Spotify Album Integration', () => {
   it('should fetch album by ID', async () => {
-    const album = await testingConductor.spotify.album.getById('1');
-    console.log('returned album:', album);
-    expect(album).toEqual(spotifySingleAlbumResponse);
+    const response =
+      await testingConductor.spotify.album.getById(spotifyAlbumIdSample);
+    expect(response).toEqual(spotifySingleAlbumResponse);
   });
 
-  // it('should fetch several albums by IDs', async () => {
-  //   const albums = await testingConductor.spotify.album.getSeveralById([
-  //     '1',
-  //     '2',
-  //   ]);
-  //   expect(albums.albums).toEqual(spotifySeveralAlbumsResponse);
-  // });
+  it('should fetch several albums by IDs', async () => {
+    const response = await testingConductor.spotify.album.getSeveralById([
+      spotifyAlbumIdSample,
+      spotifyAlbumIdSample,
+    ]);
+    expect(response).toEqual(spotifySeveralAlbumsResponse);
+  });
 
-  // it('should fetch album tracks', async () => {
-  //   const tracks = await testingConductor.spotify.album.getTracks('1');
-  //   expect(tracks.items).toEqual(spotifyAlbumTracksResponse);
-  // });
+  it('should fetch album tracks', async () => {
+    const response = await testingConductor.spotify.album.getTracks(
+      spotifyAlbumIdSample,
+      spotifyLimitAndOffsetOptionsSample,
+    );
+    expect(response).toEqual(spotifyAlbumTracksResponse);
+  });
 
-  // it("should fetch user's saved albums", async () => {
-  //   const savedAlbums =
-  //     await testingConductor.spotify.album.getUsersSavedAlbums();
-  //   expect(savedAlbums).toEqual(spotifyUsersSavedAlbumsResponse);
-  // });
+  it("should fetch user's saved albums", async () => {
+    const response = await testingConductor.spotify.album.getUsersSavedAlbums(
+      spotifyLimitAndOffsetOptionsSample,
+    );
+    expect(response).toEqual(spotifyUsersSavedAlbumsResponse);
+  });
 
-  // it('should save albums for user', async () => {
-  //   await testingConductor.spotify.album.saveAlbumsForUser(['1']);
-  //   // No assertion needed as we are just testing the request
-  // });
+  it('should save albums for user', async () => {
+    const response = await testingConductor.spotify.album.saveAlbumsForUser(
+      spotifyAlbumIdsSample,
+    );
+    expect(response).toEqual(null);
+  });
 
-  // it('should remove album for user', async () => {
-  //   await testingConductor.spotify.album.removeAlbumForUser('1');
-  //   // No assertion needed as we are just testing the request
-  // });
+  it('should remove album for user', async () => {
+    const response = await testingConductor.spotify.album.removeAlbumsForUser(
+      spotifyAlbumIdsSample,
+    );
+    expect(response).toEqual(null);
+  });
 
-  // it("should check user's saved albums", async () => {
-  //   const result = await testingConductor.spotify.album.checkUsersSavedAlbums([
-  //     '1',
-  //     '2',
-  //   ]);
-  //   expect(result).toEqual(spotifyCheckUserSavedAlbumsResponse);
-  // });
+  it("should check user's saved albums", async () => {
+    const result = await testingConductor.spotify.album.checkUsersSavedAlbums([
+      spotifyAlbumIdSample,
+      spotifyAlbumIdSample,
+    ]);
+    expect(result).toEqual(spotifyCheckUserSavedAlbumsResponse);
+  });
 });
