a52f1ba330aec0491c0a005da2c1db536a9b9bb9
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-11-06 14:33:01 +0100

    implemented validateRequest function

--- tests/utils/mockFetch/spotify/methods/artist.ts
+++ tests/utils/mockFetch/spotify/methods/artist.ts
@@ -1,3 +1,4 @@
+import { SPOTIFY_METHODS_PATHS } from '../../../../../src/conductor/providers/spotify/constants';
 import {
   spotifySingleArtistResponse,
   spotifyMultipleArtistsResponse,
@@ -5,7 +6,8 @@ import {
   spotifyArtistsTopTracksResponse,
   spotifyArtistsRelatedArtistsResponse,
 } from '../../../mockData/spotify/responses/artist';
-import { returnMockResponse } from '../../hepler';
+import { SPOTIFY_URL_PARAMS } from '../../constants';
+import { validateRequest, createMockSuccessResponse } from '../../hepler';
 
 export function handleSpotifyArtistMockRequests(
   url: string,
@@ -13,18 +15,48 @@ export function handleSpotifyArtistMockRequests(
 ) {
   if (url.match(/\/v1\/artists\/[^/]+$/)) {
     // get artist by id
-    returnMockResponse(spotifySingleArtistResponse);
+    const errorResponse = validateRequest('GET', options?.method, [], url);
+    if (errorResponse) return errorResponse;
+    return createMockSuccessResponse(spotifySingleArtistResponse);
   } else if (url.includes('/v1/artists?ids=')) {
     // get multiple artists by ids
-    returnMockResponse(spotifyMultipleArtistsResponse);
+    const errorResponse = validateRequest(
+      'GET',
+      options?.method,
+      [SPOTIFY_URL_PARAMS.ids],
+      url,
+    );
+    if (errorResponse) return errorResponse;
+    return createMockSuccessResponse(spotifyMultipleArtistsResponse);
   } else if (url.match(/\/v1\/artists\/[^/]+\/albums$/)) {
     // get artist's albums
-    returnMockResponse(spotifyArtistsAlbumsResponse);
+    const errorResponse = validateRequest(
+      'GET',
+      options?.method,
+      [
+        SPOTIFY_URL_PARAMS.market,
+        SPOTIFY_URL_PARAMS.include_groups,
+        SPOTIFY_URL_PARAMS.limit,
+        SPOTIFY_URL_PARAMS.offset,
+      ],
+      url,
+    );
+    if (errorResponse) return errorResponse;
+    return createMockSuccessResponse(spotifyArtistsAlbumsResponse);
   } else if (url.match(/\/v1\/artists\/[^/]+\/top-tracks$/)) {
     // get artist's top tracks
-    returnMockResponse(spotifyArtistsTopTracksResponse);
+    const errorResponse = validateRequest(
+      'GET',
+      options?.method,
+      [SPOTIFY_URL_PARAMS.market],
+      url,
+    );
+    if (errorResponse) return errorResponse;
+    return createMockSuccessResponse(spotifyArtistsTopTracksResponse);
   } else if (url.match(/\/v1\/artists\/[^/]+\/related-artists$/)) {
     // get related artists
-    returnMockResponse(spotifyArtistsRelatedArtistsResponse);
+    const errorResponse = validateRequest('GET', options?.method, [], url);
+    if (errorResponse) return errorResponse;
+    return createMockSuccessResponse(spotifyArtistsRelatedArtistsResponse);
   }
 }
