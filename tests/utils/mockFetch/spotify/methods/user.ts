463d1b85edecc9fee346b017a2840c28f8139875
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-11-06 14:32:22 +0100

    implemented validateResponse function

--- tests/utils/mockFetch/spotify/methods/user.ts
+++ tests/utils/mockFetch/spotify/methods/user.ts
@@ -6,7 +6,8 @@ import {
   spotifyUserFollowsUsersOrArtistsResponse,
   spotifyUsersFollowPlaylistResponse,
 } from '../../../mockData/spotify/responses/user';
-import { returnMockResponse } from '../../hepler';
+import { SPOTIFY_URL_PARAMS } from '../../constants';
+import { createMockSuccessResponse, validateRequest } from '../../hepler';
 
 export async function handleSpotifyUserMockRequests(
   url: string,
@@ -15,55 +16,92 @@ export async function handleSpotifyUserMockRequests(
   // Users
   if (url.match(/\/v1\/users\/[^/]+$/) && options?.method === 'GET') {
     // get user by id
-    return returnMockResponse(spotifyUserProfileResponse);
+    const errorResponse = validateRequest('GET', options?.method, [], url);
+    if (errorResponse) return errorResponse;
+    return createMockSuccessResponse(spotifyUserProfileResponse);
   } else if (url.includes('/v1/me') && options?.method === 'GET') {
     // get current user
-    return returnMockResponse(spotifyCurrentUsersProfileResponse);
+    const errorResponse = validateRequest('GET', options?.method, [], url);
+    if (errorResponse) return errorResponse;
+    return createMockSuccessResponse(spotifyCurrentUsersProfileResponse);
   } else if (
     url.match(/\/v1\/me\/top\/(artists|tracks)$/) &&
     options?.method === 'GET'
   ) {
     // get current user's top items
+    const errorResponse = validateRequest(
+      'GET',
+      options?.method,
+      [
+        SPOTIFY_URL_PARAMS.type,
+        SPOTIFY_URL_PARAMS.time_range,
+        SPOTIFY_URL_PARAMS.limit,
+        SPOTIFY_URL_PARAMS.offset,
+      ],
+      url,
+    );
+    if (errorResponse) return errorResponse;
     if (url.includes('artists')) {
-      return returnMockResponse(spotifyUsersTopArtistsResponse);
+      return createMockSuccessResponse(spotifyUsersTopArtistsResponse);
     } else if (url.includes('tracks')) {
-      return returnMockResponse(spotifyUsersTopTracksResponse);
+      return createMockSuccessResponse(spotifyUsersTopTracksResponse);
     }
   } else if (
     url.match(/\/v1\/playlists\/[^/]+\/followers$/) &&
     options?.method === 'PUT'
   ) {
     // follow playlist
-    return returnMockResponse(null);
+    const errorResponse = validateRequest('PUT', options?.method, [], url);
+    if (errorResponse) return errorResponse;
+    return createMockSuccessResponse(null);
   } else if (
     url.match(/\/v1\/playlists\/[^/]+\/followers$/) &&
     options?.method === 'DELETE'
   ) {
     // unfollow playlist
-    return returnMockResponse(null);
-  } else if (
-    url.includes('/v1/me/following?type=artist') &&
-    options?.method === 'PUT'
-  ) {
+    const errorResponse = validateRequest('DELETE', options?.method, [], url);
+    if (errorResponse) return errorResponse;
+    return createMockSuccessResponse(null);
+  } else if (url.includes('/v1/me/following') && options?.method === 'PUT') {
     // follow artist
-    return returnMockResponse(null);
-  } else if (
-    url.includes('/v1/me/following?type=artist') &&
-    options?.method === 'DELETE'
-  ) {
+    const errorResponse = validateRequest(
+      'PUT',
+      options?.method,
+      [SPOTIFY_URL_PARAMS.type],
+      url,
+    );
+    if (errorResponse) return errorResponse;
+    return createMockSuccessResponse(null);
+  } else if (url.includes('/v1/me/following') && options?.method === 'DELETE') {
     // unfollow artist
-    return returnMockResponse(null);
+    const errorResponse = validateRequest(
+      'DELETE',
+      options?.method,
+      [SPOTIFY_URL_PARAMS.type],
+      url,
+    );
+    if (errorResponse) return errorResponse;
+    return createMockSuccessResponse(null);
   } else if (
-    url.includes('/v1/me/following/contains?type=artist') &&
+    url.includes('/v1/me/following/contains') &&
     options?.method === 'GET'
   ) {
     // check if follows artists or users
-    return returnMockResponse(spotifyUserFollowsUsersOrArtistsResponse);
+    const errorResponse = validateRequest(
+      'GET',
+      options?.method,
+      [SPOTIFY_URL_PARAMS.ids, SPOTIFY_URL_PARAMS.type],
+      url,
+    );
+    if (errorResponse) return errorResponse;
+    return createMockSuccessResponse(spotifyUserFollowsUsersOrArtistsResponse);
   } else if (
     url.match(/\/v1\/playlists\/[^/]+\/followers\/contains$/) &&
     options?.method === 'GET'
   ) {
     // check if user follows playlist
-    return returnMockResponse(spotifyUsersFollowPlaylistResponse);
+    const errorResponse = validateRequest('GET', options?.method, [], url);
+    if (errorResponse) return errorResponse;
+    return createMockSuccessResponse(spotifyUsersFollowPlaylistResponse);
   }
 }
