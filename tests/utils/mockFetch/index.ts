26faa198ad3819caee20b6ec5d6c76092721829c
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-11-07 20:48:52 +0100

    Refactor mockFetch to handle Apple Music API requests

--- tests/utils/mockFetch/index.ts
+++ tests/utils/mockFetch/index.ts
@@ -1,5 +1,7 @@
 import { SPOTIFY_API_BASE_URL } from './spotify/constants';
 import { handleSpotifyMockFetch } from './spotify';
+import { APPLE_MUSIC_API_BASE_URL } from './appleMusic/costants';
+import { handleAppleMusicMockFetch } from './appleMusic';
 
 export async function mockFetch(url: string, options?: RequestInit) {
   console.log('mockFetch url', url);
@@ -7,5 +9,7 @@ export async function mockFetch(url: string, options?: RequestInit) {
 
   if (url.includes(SPOTIFY_API_BASE_URL)) {
     return handleSpotifyMockFetch(url, options);
+  } else if (url.includes(APPLE_MUSIC_API_BASE_URL)) {
+    return handleAppleMusicMockFetch(url, options);
   }
 }
