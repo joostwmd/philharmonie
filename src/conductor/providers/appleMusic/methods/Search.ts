b1d02b24531764b3e2606430cf0e69338a47ed76
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-11-07 20:48:38 +0100

    Refactor Search class to accept additional search item types and parameters

--- src/conductor/providers/appleMusic/methods/Search.ts
+++ src/conductor/providers/appleMusic/methods/Search.ts
@@ -2,13 +2,25 @@ import type { AppleMusicConductorProvider } from '..';
 import { APPLE_MUSIC_BASE_URL, APPLE_MUSIC_METHODS_PATHS } from '../constants';
 import type { SearchResultsResponse } from '../types/response';
 
+type TSearchItemType =
+  | 'activities'
+  | 'albums'
+  | 'apple-curators'
+  | 'artists'
+  | 'curators'
+  | 'music-videos'
+  | 'playlists'
+  | 'record-labels'
+  | 'songs'
+  | 'stations';
+
 export type TSearchCatalogItemInput = {
   term: string;
-  types: string[];
+  types: TSearchItemType[];
   limit?: number;
-  offset?: string;
-  localization?: string;
-  with?: string[];
+  offset?: number;
+  l?: string;
+  with?: 'topResults';
 };
 
 export class Search {
@@ -21,29 +33,21 @@ export class Search {
   async searchCatalog(
     input: TSearchCatalogItemInput,
   ): Promise<SearchResultsResponse> {
-    const {
-      term,
-      types,
-      limit = 5,
-      offset,
-      localization,
-      with: withParam,
-    } = input;
+    const { term, types, limit, offset, l, with: withParam } = input;
 
     let url = `${APPLE_MUSIC_BASE_URL}${APPLE_MUSIC_METHODS_PATHS.catalog}${this.provider.market}/${APPLE_MUSIC_METHODS_PATHS.search}`;
 
     const params: Record<string, string> = {
       term: term.replace(/\s+/g, '+'),
       types: types.join(','),
-      limit: limit.toString(),
     };
 
-    if (offset) params.offset = offset;
-    if (localization) params.l = localization;
-    if (withParam) params.with = withParam.join(',');
+    if (limit) params.limit = limit.toString();
+    if (offset) params.offset = offset.toString();
+    if (l) params.l = l;
+    if (withParam) params.with = withParam;
 
     url = this.provider.injectParamsIntoUrl(url, params);
-
     return await this.provider.makeRequest(url, 'GET');
   }
 }
